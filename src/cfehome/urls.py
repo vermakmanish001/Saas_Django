80c438f402c3629ab198ad47dd87f1d811efab46
Manish Verma <your.vermakmanish001@egmail.com>
2024-11-07 00:28:05 +0530

    section 1

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_view
+from .views import home_view, about_view
 
 urlpatterns = [
-    path("",home_view),
-    path('admin/', admin.site.urls),
+    path("", home_view), #index page -> root page
+    path("about/", about_view),
+    path("hello-world/", home_view),
+    path("hello-world.html", home_view),
+    path("admin/", admin.site.urls),
 ]
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,12 +1,14 @@
 import pathlib
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+
 from visits.models import PageVisit
 
+this_dir = pathlib.Path(__file__).resolve().parent
+
 def home_view(request, *args, **kwargs):
-    if request.user.is_authenticated:
-        print(request.user.first_name)
     return about_view(request, *args, **kwargs)
+
 
 def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
@@ -25,3 +27,22 @@ def about_view(request, *args, **kwargs):
     }
     PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
+
+
+def my_old_home_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_ = """
+    <!DOCTYPE html>
+<html>
+
+<body>
+    <h1>{page_title} anything?</h1>
+</body>
+</html>    
+""".format(**my_context) # page_title=my_title
+    # html_file_path = this_dir / "home.html"
+    # html_ = html_file_path.read_text()
+    return HttpResponse(html_)
