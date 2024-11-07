ffed0a56774e37b3f221e7ba86f9f1e283b7f22f
Manish Verma <your.vermakmanish001@egmail.com>
2024-11-07 18:53:51 +0530

    dd

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-fg)#tp4kt77h(lci=3i$ssd2^vr(3sh5lau=x#1#(svfzf!9v&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".railway.app" # https://saas.prod.railway.app
